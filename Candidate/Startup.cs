af56af043b64751b49e1091ab0af63534a0121a9
Aaron Lenhart <aaron@Aarons-MacBook-Air.local>
2024-05-18 15:57:36 -0400

    naming namespace

--- Candidate/Startup.cs
+++ Candidate/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Hosting;
 using Candidate;
 using Services;// Import the OrganizationServices namespace
 
-namespace YourNamespace
+namespace Candidate
 {
     public class Startup
     {
